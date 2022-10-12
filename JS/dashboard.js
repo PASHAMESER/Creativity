2c07644bc975e8f191f1222d280df834f8f918af
Mohamed Roshdy <62522577+PASHAMESER@users.noreply.github.com>
2022-10-12 22:05:18 +0200

    up

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,56 +1,52 @@
+let deviconColor = document.querySelector(".deviconColor");
+let devitmeColor = document.querySelector(".devitmeColor");
 
+deviconColor.addEventListener("click", () => {
+  devitmeColor.classList.toggle("showItmeColor")
+});
 
-let deviconColor = document.querySelector(".deviconColor")
-let devitmeColor = document.querySelector(".devitmeColor")
+let color1 = document.getElementById("color1");
+let color2 = document.getElementById("color2");
+let color3 = document.getElementById("color3");
 
-deviconColor.addEventListener('click' , ()=>{
-    devitmeColor.classList.toggle("showItmeColor")
-})
+let containerNav = document.querySelector(".containerNav");
+let rightcontrolcontainer = document.querySelector(".rightcontrolcontainer");
+let ContainerPage = document.querySelector(".ContainerPage");
+let boxAreport = document.querySelector(".boxAreport");
+let boxone = document.querySelector(".boxone");
+let boxtow = document.querySelector(".boxtow");
+let viweAll = document.querySelector("#viweAll");
+let devbgIcon = document.querySelectorAll(".devbg-icon");
+let forBox = document.querySelectorAll(".box");
 
-let color1 = document.getElementById("color1")
-let color2 = document.getElementById("color2")
-let color3 = document.getElementById("color3")
-
-let containerNav = document.querySelector(".containerNav")
-let rightcontrolcontainer = document.querySelector(".rightcontrolcontainer")
-let ContainerPage = document.querySelector(".ContainerPage")
-let boxAreport = document.querySelector(".boxAreport")
-let boxone = document.querySelector(".boxone")
-let boxtow = document.querySelector(".boxtow")
-let viweAll = document.querySelector("#viweAll")
-let devbgIcon = document.querySelectorAll(".devbg-icon")
-let forBox = document.querySelectorAll(".box")
-
-color1.onclick = ()=>{
-    forBox.forEach(itme => {
-        itme.classList.remove("activecontinerdevboxse")
-        devbgIcon.forEach(itmeIcon =>{
-            itmeIcon.classList.remove("activecontinerdevicon")
-        })
+color1.onclick = () => {
+  forBox.forEach((itme) => {
+    itme.classList.remove("activecontinerdevboxse");
+    devbgIcon.forEach((itmeIcon) => {
+      itmeIcon.classList.remove("activecontinerdevicon");
     });
-    ContainerPage.classList.remove("activecontiner")
-    boxAreport.classList.remove("activecontinerdevboxse")
-    boxone.classList.remove("activecontinerStar")
-    boxtow.classList.remove("activecontinerdevboxse")
-    containerNav.classList.remove("activecontinerdevboxse")
-    rightcontrolcontainer.classList.remove("activecontinerdevboxse")
-    viweAll.classList.remove("activecontinerdevBtn")
-}
+  });
+  ContainerPage.classList.remove("activecontiner");
+  boxAreport.classList.remove("activecontinerdevboxse");
+  boxone.classList.remove("activecontinerStar");
+  boxtow.classList.remove("activecontinerdevboxse");
+  containerNav.classList.remove("activecontinerdevboxse");
+  rightcontrolcontainer.classList.remove("activecontinerdevboxse");
+  viweAll.classList.remove("activecontinerdevBtn");
+};
 
-color2.onclick = ()=>{
-    forBox.forEach(itme => {
-        itme.classList.add("activecontinerdevboxse")
-        devbgIcon.forEach(itmeIcon =>{
-            itmeIcon.classList.add("activecontinerdevicon")
-        })
+color2.onclick = () => {
+  forBox.forEach((itme) => {
+    itme.classList.add("activecontinerdevboxse");
+    devbgIcon.forEach((itmeIcon) => {
+      itmeIcon.classList.add("activecontinerdevicon");
     });
-    ContainerPage.classList.add("activecontiner")
-    boxAreport.classList.add("activecontinerdevboxse")
-    boxone.classList.add("activecontinerStar")
-    boxtow.classList.add("activecontinerdevboxse")
-    containerNav.classList.add("activecontinerdevboxse")
-    rightcontrolcontainer.classList.add("activecontinerdevboxse")
-    viweAll.classList.add("activecontinerdevBtn")
-    
-    
-}
+  });
+  ContainerPage.classList.add("activecontiner");
+  boxAreport.classList.add("activecontinerdevboxse");
+  boxone.classList.add("activecontinerStar");
+  boxtow.classList.add("activecontinerdevboxse");
+  containerNav.classList.add("activecontinerdevboxse");
+  rightcontrolcontainer.classList.add("activecontinerdevboxse");
+  viweAll.classList.add("activecontinerdevBtn");
+};
